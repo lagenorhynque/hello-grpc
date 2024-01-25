442a8b5226a1dbb5275cd461db36025e8560c82c
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2024-01-25 14:56:28 +0900

    Update dependencies

--- project.clj
+++ project.clj
@@ -5,20 +5,20 @@
             :url "https://www.apache.org/licenses/LICENSE-2.0"
             :year 2022
             :key "apache-2.0"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
-                 [io.pedestal/pedestal.service "0.5.9"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [io.pedestal/pedestal.service "0.6.3"]
 
                  ;; -- PROTOC-GEN-CLOJURE --
-                 [io.github.protojure/grpc-server "2.0.1"]
-                 [io.github.protojure/google.protobuf "2.0.0"]
+                 [io.github.protojure/grpc-server "2.8.1"]
+                 [io.github.protojure/google.protobuf "2.0.1"]
 
-                 [ch.qos.logback/logback-classic "1.2.9"]
-                 [org.slf4j/jul-to-slf4j "1.7.32"]
-                 [org.slf4j/jcl-over-slf4j "1.7.32"]
-                 [org.slf4j/log4j-over-slf4j "1.7.32"]]
+                 [ch.qos.logback/logback-classic "1.4.14"]
+                 [org.slf4j/jul-to-slf4j "2.0.11"]
+                 [org.slf4j/jcl-over-slf4j "2.0.11"]
+                 [org.slf4j/log4j-over-slf4j "2.0.11"]]
   :min-lein-version "2.0.0"
   :resource-paths ["config", "resources"]
   :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "hello-grpc.server/run-dev"]}
-                   :dependencies [[io.pedestal/pedestal.service-tools "0.5.9"]]}
+                   :dependencies [[io.pedestal/pedestal.service-tools "0.6.3"]]}
              :uberjar {:aot [hello-grpc.server]}}
   :main ^{:skip-aot true} hello-grpc.server)
